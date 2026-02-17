532bd4e058ca390fcef462edaf2b1f0bfe13adeb
Giovanni Pollo <giovannipollo98@gmail.com>
2026-02-17 16:18:13 +0100

    Removed config validator for phases

--- utils/__init__.py
+++ utils/__init__.py
@@ -25,9 +25,6 @@ from .fault_injection import (
     QuantActivationFaultInjectionLayer
 )
 
-# Multi-phase training
-from .phase_config import PhaseConfig
-from .phase_manager import PhaseManager, create_phase_manager
 from .config_validator import ConfigValidator, validate_config, ConfigValidationError
 
 __all__ = [
@@ -48,10 +45,6 @@ __all__ = [
     "ActivationFaultInjector",
     "FaultStatistics",
     "QuantActivationFaultInjectionLayer",
-    # Multi-phase training
-    "PhaseConfig",
-    "PhaseManager",
-    "create_phase_manager",
     # Configuration validation
     "ConfigValidator",
     "validate_config",
